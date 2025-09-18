3539ec0b9f3e4cb9aae4359fff125e92813d9095
Ebubee-O <iamokygoodluck@gmail.com>
2025-09-18 05:34:59 -0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  background-color: #f3f4f6;
+}
