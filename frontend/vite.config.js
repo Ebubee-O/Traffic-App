af4cebffa23e14c79d427b4732f6191c593bd933
Ebubee-O <iamokygoodluck@gmail.com>
2025-09-18 06:11:26 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    allowedHosts: ['7caccdff-c5e3-4a7d-a25b-d2bd203e1562-00-2wpjvlnex50ng.spock.replit.dev'],
   },
   build: {
     outDir: 'dist',
